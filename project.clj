61f070d5823eac5b528e8a1f4e59a82dbe811a36
Manfred Endres <manfred.endres@tslarusso.de>
2017-02-06 16:34:27 +0100

    latest changes

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject
   uochan/watchtower "0.1.4"
-  :description "A library for directory watchers forked by ibdknox/watchtower"
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :description "A library for directory watchers forked by liquidz/watchtower"
+  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :plugins [[s3-wagon-private "1.1.2"]]
+  :repositories [["private" {:url "s3p://larusso-clojars/releases" :creds :gpg}]])
